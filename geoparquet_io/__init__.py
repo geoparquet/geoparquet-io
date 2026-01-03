9ba403f3a7498d56d7b5f57b7faf9dd298299b1c
Chris Holmes <cholmes@planet.com>
2026-01-03 14:34:06 +0100

    Export convert and ops from top-level module

--- geoparquet_io/__init__.py
+++ geoparquet_io/__init__.py
@@ -1,4 +1,4 @@
-from geoparquet_io.api import Table, pipe, read
+from geoparquet_io.api import Table, convert, ops, pipe, read
 from geoparquet_io.cli.main import cli
 
-__all__ = ["cli", "read", "Table", "pipe"]
+__all__ = ["cli", "read", "convert", "Table", "pipe", "ops"]
